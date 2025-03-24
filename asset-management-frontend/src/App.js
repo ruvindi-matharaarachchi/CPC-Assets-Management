6459567872ede7ede0773af90057940cbefd3b47
Ruvindi Chathurya <133088416+ruvindi-matharaarachchi@users.noreply.github.com>
2025-03-24 10:57:10 +0530

    Update App.js

--- asset-management-frontend/src/App.js
+++ asset-management-frontend/src/App.js
@@ -6,6 +6,7 @@ import AddAsset from "./components/AddAsset";
 import AssetList from "./components/AssetList";
 import ViewAsset from "./components/ViewAsset";
 import AddCommonAsset from "./pages/AddCommonAsset";
+import AddAssetForm from "./components/AddAssetForm";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/assets" element={<AssetList />} />  {/* Asset List Page */}
         <Route path="/assets/:id" element={<ViewAsset />} /> {/* View Asset Page */}
         <Route path="/add-common-asset" element={<AddCommonAsset />} />
+        <Route path="/add-asset-form" element={<AddAssetForm />} /> {/* ✅ New Route */}
 
       </Routes>
     </Router>
