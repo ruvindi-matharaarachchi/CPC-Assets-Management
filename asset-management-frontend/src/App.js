e5ea234ed2b960cdf316dcf0c52bdce1426c7f11
Ruvindi Chathurya <133088416+ruvindi-matharaarachchi@users.noreply.github.com>
2025-03-26 11:49:35 +0530

    ww

--- asset-management-frontend/src/App.js
+++ asset-management-frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import ViewAsset from "./components/ViewAsset";
 import AddCommonAsset from "./pages/AddCommonAsset";
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/assets/:id" element={<ViewAsset />} /> {/* View Asset Page */}
         <Route path="/add-common-asset" element={<AddCommonAsset />} />
 
       </Routes>
