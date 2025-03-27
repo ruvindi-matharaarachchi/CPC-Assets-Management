f2753e4573af69e60e4044e096291a10486ef827
Ruvindi Chathurya <133088416+ruvindi-matharaarachchi@users.noreply.github.com>
2025-03-27 15:44:20 +0530

    Update server.js

--- asset-management-backend/server.js
+++ asset-management-backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const assetDetailsRoutes = require('./routes/asset-details');
 
 dotenv.config();
 
