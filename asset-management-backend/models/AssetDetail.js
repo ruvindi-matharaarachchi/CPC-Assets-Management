cb39c8be4f03cc2de557026fda87c9de874e7160
Ruvindi Chathurya <133088416+ruvindi-matharaarachchi@users.noreply.github.com>
2025-03-27 11:51:38 +0530

    Update AssetDetail.js

--- asset-management-backend/models/AssetDetail.js
+++ asset-management-backend/models/AssetDetail.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const assetDetailSchema = new mongoose.Schema({
   commonAssetId: { type: mongoose.Schema.Types.ObjectId, ref: "CommonAsset", required: true },
-  serialNumber: { type: String, required: true },
   assignedTo: { type: String },
   location: { type: String, required: true },
+  serialNumber: { type: String, required: true },
   assetNumber: { type: String },
   remarks: { type: String },
   createdAt: { type: Date, default: Date.now },
