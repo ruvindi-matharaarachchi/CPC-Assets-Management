e94da77b56d6cc70d0bbb7fe4e4b279da5004ddf
Ruvindi Chathurya <133088416+ruvindi-matharaarachchi@users.noreply.github.com>
2025-04-19 17:23:55 +0530

    Update technicians.js

--- asset-management-backend/routes/technicians.js
+++ asset-management-backend/routes/technicians.js
@@ -13,5 +13,15 @@ router.post("/add", async (req, res) => {
     res.status(500).json({ message: "Server error", error: err.message });
   }
 });
-
+// GET all technicians
+router.get("/", async (req, res) => {
+    try {
+      const techs = await Technician.find().sort({ createdAt: -1 });
+      res.json(techs);
+    } catch (err) {
+      console.error("Error fetching technicians:", err);
+      res.status(500).json({ message: "Server error" });
+    }
+  });
+  
 module.exports = router;
